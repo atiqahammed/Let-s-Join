df8779ef089e6dafe849dbe4c543a836849d3434
Atiq Ahammed <atiqahammedshamim@gmail.com>
2021-01-11 20:59:31 +0600

    video is rendering on the UI

---
+++ public/script.js
@@ -0,0 +1,21 @@
+const videoGrid = document.getElementById('video-grid');
+const userVideo = document.createElement('video');
+userVideo.muted = true;
+
+let userVideoStream;
+ 
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    userVideoStream = stream;
+    addVideoStream(userVideo, userVideoStream);
+});
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
