5937467be372972b3a5e3e327ca30d459527667d
Atiq Ahammed <atiqahammedshamim@gmail.com>
2021-01-11 18:16:54 +0600

    connected with back forntend through ejs

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
  
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.status(200).send('Let\'s Join');
+    res.render('room');
 });
 
 server.listen(3030);
